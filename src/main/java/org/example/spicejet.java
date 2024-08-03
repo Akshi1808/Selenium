ff39a5a300defacd68bb6ecf56a32b429fac9e2f
Akshi1808 <119392437+Akshi1808@users.noreply.github.com>
2024-08-03 17:49:59 +0530

    selenium

--- src/main/java/org/example/spicejet.java
+++ src/main/java/org/example/spicejet.java
@@ -2,14 +2,18 @@ package org.example;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class spicejet {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static <list> void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "C:\\driver\\chromedriver-win64\\chromedriver.exe");
         WebDriver driver = new ChromeDriver();
@@ -21,15 +25,31 @@ public class spicejet {
         driver.get("https://www.spicejet.com/");
         driver.manage().timeouts().implicitlyWait(Duration.ofDays(60));
 
-        driver.findElement(By.xpath("//div[@class = 'css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep']/input[1]")).sendKeys("AGR");
-        Thread.sleep(200);
+        List<WebElement> elements = driver.findElements(By.xpath("//div[@class = 'css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep']/input[1]"));
 
-//        driver.findElement(By.xpath("//div[@class = 'to-testID-destination']/div/div[1]")).click();
-        driver.findElement(By.xpath("//div[@class = 'css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep']/input[1][2]")).sendKeys("JAI");
-        Thread.sleep(400);
+        for (WebElement element : elements) {
+            System.out.println(element.getText());
+            // Perform actions like click, sendKeys, etc.
+        }
 
-        driver.findElement(By.className("css-1dbjc4n r-1awozwy r-18u37iz r-1wtj0ep]/div[1]")).click();
-        Thread.sleep(2000);
+        WebElement firstElement = elements.get(0);
+        firstElement.sendKeys("AGR");
+
+        WebElement secondElement = elements.get(1);
+        secondElement.sendKeys("JSA");
+
+        Thread.sleep(1000);
+
+        List<WebElement> date = driver.findElements(By.xpath("//div[@class = 'css-1dbjc4n']/div/div[3]"));
+
+        WebElement futuredate = date.get(5);
+        System.out.println(futuredate);
+        futuredate.click();
+
+        Thread.sleep(1000);
+
+        driver.findElement(By.xpath("//div[@class = 'css-1dbjc4n r-18u37iz r-1wtj0ep r-1lgpqti']/div[2]/div[1]")).click();
+        Thread.sleep(3000);
 
         driver.close();
 
