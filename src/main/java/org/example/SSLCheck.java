b9a298546da79a3e496e9bf934c424b04a8753a2
Akshi1808 <119392437+Akshi1808@users.noreply.github.com>
2024-08-24 16:12:57 +0530

    selenium

--- src/main/java/org/example/SSLCheck.java
+++ src/main/java/org/example/SSLCheck.java
@@ -13,7 +13,7 @@ public class SSLCheck {
 
     public static void main(String[] args) {
 
-// TODO Auto-generated method stub
+        // TODO Auto-generated method stub
 
         ChromeOptions options = new ChromeOptions();
 
